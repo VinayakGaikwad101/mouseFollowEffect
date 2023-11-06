68a3a96d1ec56d05cc0d4fd111c39bd16b7402d1
VinayakGaikwad <93867583+VinayakGaikwad101@users.noreply.github.com>
2023-11-06 21:30:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const blob = document.querySelector("#blob");
+
+document.body.onpointermove = (event) => {
+  const { clientX, clientY } = event;
+  blob.style.left = `${clientX}px`;
+  blob.style.top = `${clientY}px`;
+};
+
+blob.animate(
+  {
+    left: `${clientX}px`,
+    top: `${clientY}px`,
+  },
+  {
+    duration: 3000,
+    fill: "forwards",
+  }
+);
